e58173f11a24f9c5ff8f4c3ee820998852216c92
doclapin <sim_conejo@yahoo.fr>
2016-01-09 12:14:42 +0100

    Ajout d'une limite de zoom et de la carte

--- web/carte.js
+++ web/carte.js
@@ -1,4 +1,14 @@
-var map = L.map('map').setView([48.858, 2.335], 13);
+//limites de la carte
+var limiteBasDroite = L.latLng(48.791776, 2.451451),
+    limiteHautGauche = L.latLng(48.926351, 2.224334);
+
+var map = L.map('map', {
+  center: [48.858, 2.335],
+  zoom: 13,
+  minZoom: 12,
+  maxZoom: 14,
+  maxBounds: L.latLngBounds(limiteBasDroite, limiteHautGauche)
+})
 
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -10,6 +20,7 @@ g = svg.append("g").attr("class", "leaflet-zoom-hide");
 var color = d3.scale.linear()
 .range(["white", "red"]);
 
+//charger les arrondissemnts
 d3.json("paris.json", function(error, collection) {
   if (error) {
     alert('Erreur lors du chargement des arrondissements');
@@ -19,22 +30,22 @@ d3.json("paris.json", function(error, collection) {
   arrdt = d3.geo.path().projection(transform);
 
   var feature = g.selectAll("path")
-      .data(collection.features)
-    .enter().append("path");
+  .data(collection.features)
+  .enter().append("path");
 
-  map.on("viewreset", reset);
-  reset();
+  map.on("viewreset", affichageDynamique);
+  affichageDynamique();
 
-  // Reposition the SVG to cover the features.
-  function reset() {
+  // fonction permettant d'afficher les arrdt dynamiquement
+  function affichageDynamique() {
     var bounds = arrdt.bounds(collection),
-        topLeft = bounds[0],
-        bottomRight = bounds[1];
+    topLeft = bounds[0],
+    bottomRight = bounds[1];
 
     svg .attr("width", bottomRight[0] - topLeft[0])
-        .attr("height", bottomRight[1] - topLeft[1])
-        .style("left", topLeft[0] + "px")
-        .style("top", topLeft[1] + "px");
+    .attr("height", bottomRight[1] - topLeft[1])
+    .style("left", topLeft[0] + "px")
+    .style("top", topLeft[1] + "px");
 
     g   .attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
 
@@ -46,5 +57,8 @@ d3.json("paris.json", function(error, collection) {
     var point = map.latLngToLayerPoint(new L.LatLng(y, x));
     this.stream.point(point.x, point.y);
   }
+
 });
+
+
 
